9668134cf5d7681d8bb1380f7dc64e41ce0ed7c5
ElJdP <68053570+ElJdP@users.noreply.github.com>
2022-03-10 17:23:14 +0100

    Update and rename private_example.js to private.js

--- private.js
+++ private.js
@@ -1,3 +1,3 @@
 // Get a token from https://discordapp.com/developers/applications/me
 // Replace YOUR TOKEN HERE with it and save as "private.js"
-module.exports.token = "YOUR TOKEN HERE";
+module.exports.token = "OTUxNTE0ODc4MDc2NjQ5NTQz.YiolSQ.fY_Q_nDwzV7APwZ_d0XBJQNoY78";
